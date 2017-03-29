6a7848264c665b766bbec125056883360f46afe9
Conwak <conwak@hotmail.com>
2017-03-29 20:52:45 +0100

    Textures and Lighting

---
+++ Fps_8Bit_Project/Assets/Scripts/GunClamp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunClamp : MonoBehaviour {
+
+    private float rotationZ = 0f;
+    private float sensitivityZ = 2f;
+
+    void Update () {
+        LockedRotation();
+    }
+
+	void LockedRotation () {
+        rotationZ += Input.GetAxis("Mouse Y");
+        rotationZ = Mathf.Clamp(0, 0, 0);
+
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, -rotationZ);
+    }
+}
